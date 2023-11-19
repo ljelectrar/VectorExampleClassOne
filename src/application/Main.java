44cbdbab6449b70e2a5d0b21943631c3af01cf06
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-11-19 10:56:35 -0300

    Usando Length para acessar o tamanho do vetor.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,42 @@
+package application;
+import entities.Produto;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira o tamanho do Vetor: ");
+        int n = sc.nextInt();
+
+        Produto[] products = new Produto[n];
+
+        for (int i = 0; i < products.length; i++) {
+            System.out.println("Digite o nome do produto: ");
+            sc.nextLine();
+            String nome = sc.nextLine();
+            System.out.println("Digite o preço do produto: ");
+            double preco = sc.nextDouble();
+
+            products[i] = new Produto(nome, preco);
+        }
+
+        double soma = 0.0;
+        for (int i = 0; i < products.length; i++) {
+            soma += products[i].getPreco();
+        }
+
+        for (int i = 0; i < products.length; i++) {
+            System.out.println("\n" + products[i]);
+        }
+
+        double avg = soma / n;
+        System.out.printf("Preço médio: %.2f%n", avg );
+
+        sc.close();
     }
 }
